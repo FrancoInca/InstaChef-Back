ea402131fc048c7958b70e8fa4df6f27ac67f75b
Franco Inca <francoinca1604@gmail.com>
2023-06-06 15:57:14 -0500

    fix(Routes/user): Changed "put" to "post" to create a product

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,17 +1,17 @@
-const { Router } =  require("express");
+const { Router } = require('express');
 const usersRouter = Router();
 const {
   handleUsersAll,
   handleDeleteUser,
   handleUserById,
   handleUserCreate,
-  handleUpdateUser
-} = require("../handlers/usersHandler");
+  handleUpdateUser,
+} = require('../handlers/usersHandler');
 
-usersRouter.put("/", handleUserCreate);
-usersRouter.get("/", handleUsersAll);
-usersRouter.get("/:userId", handleUserById);
-usersRouter.put("/", handleUpdateUser);
-usersRouter.delete("/:userId", handleDeleteUser)
+usersRouter.post('/', handleUserCreate);
+usersRouter.get('/', handleUsersAll);
+usersRouter.get('/:userId', handleUserById);
+usersRouter.put('/', handleUpdateUser);
+usersRouter.delete('/:userId', handleDeleteUser);
 
 module.exports = usersRouter;
