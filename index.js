425769293b774eebb8eedb6289cfa9edecc1247b
Franco Inca <francoinca1604@gmail.com>
2023-06-06 16:21:31 -0500

    feat(index): Added bulkProducts function

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const server = require('./src/app.js');
 
 const { PORT } = process.env;
 const { conn } = require('./src/db.js');
+const { bulkProducts } = require('./src/functions/bulkProducts.js');
 
 conn.sync({ force: true }).then(() => {
-
   server.listen(PORT, () => {
+    bulkProducts();
     console.log('Server is listening at ' + PORT);
   });
-
 });
