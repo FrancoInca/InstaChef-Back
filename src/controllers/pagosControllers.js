fccfac39fdd0beac5fdeabbb1b2da4658661d842
Franco Inca <francoinca1604@gmail.com>
2023-06-11 21:41:49 -0500

    feat(pagosControllers): Added .env and changed functions.

--- src/controllers/pagosControllers.js
+++ src/controllers/pagosControllers.js
@@ -1,34 +1,22 @@
+const Stripe = require('stripe');
+require("dotenv").config
+const {STRIPE_SECRET_KEY} = process.env;
 
+const stripe = new Stripe(STRIPE_SECRET_KEY);
 
-const Stripe = require("stripe")
-
-const STRIPE_SECRET_KEY = "sk_test_51N3WCTG4n6v6zt1DpsOBG2uM3p6GuLJHiOOxW8nEiEBF8pc4DuT7VPRkwo9fLYb7QrAWinkfxb1mtXMSfo2FQgcM00LlaDchu1";
-
-
-
-const stripe = new Stripe(STRIPE_SECRET_KEY)
-
-const pagos = async (req, res, next) => {
-    try {
-      let  { amount, id, email, nombre, idCurso} = req.body
-      
-        const payment = await stripe.paymentIntents.create({
-        amount,
-         currency: "USD",
-         description: "prueba",
-         payment_method: id,
-         confirm: true
-        })
-
-       
-       
-        console.log(payment)
-   
-    } catch (error) {
-        console.log(error.message);
-        next(error);
-    }
+const pagos = async (amount, id, products) => {
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: 'USD',
+      description: products,
+      payment_method: id,
+      confirm: true,
+    });
+    return payment
+  } catch (error) {
+    console.log(error.message);
+  }
 };
-
 
 module.exports = pagos;
