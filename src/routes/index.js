11d4b6fd7617817e61632ce5f2bb378f815c465e
Guido <guidofraticelli22@gmail.com>
2023-06-13 11:09:54 -0300

    deleteImg from id

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const usersRouter = require('./user');
 const productsRouter = require('./products');
 const loginRouter = require('./login');
 const pagosRouter = require('./routerPago');
+const deleteImage = require('../handlers/imageHandler');
 const router = express.Router();
 
 router.use('/users', usersRouter);
 router.use('/products', productsRouter);
 router.use('/login', loginRouter )
 router.use("/", pagosRouter)
+router.use('/:public_id', deleteImage);
 
 module.exports = router;
