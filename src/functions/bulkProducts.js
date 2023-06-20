330ae362a11be56bea85073b95fdf3042f21bf43
Franco Inca <francoinca1604@gmail.com>
2023-06-20 13:57:14 -0500

    feat(bulk): Added Admin Role

--- src/functions/bulkProducts.js
+++ src/functions/bulkProducts.js
@@ -1,9 +1,16 @@
-const { Product } = require('../db');
+const { Product, User, Role } = require('../db');
 const { comidas } = require('../utils/Productos');
 
 const bulkProducts = async () => {
   try {
     await Product.bulkCreate(comidas);
+    await Role.create({id: 1, name: "Admin", user_roles: 1})
+    await User.create({
+      name: "Admin",
+      email: "chef@insta.com",
+      password: "Chef1234",
+      user_roles: 1
+    })
     console.log('Database is ready');
   } catch (error) {
     console.log('DB has an error..');
